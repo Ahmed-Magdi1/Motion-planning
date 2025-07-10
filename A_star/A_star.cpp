8afea6c9f2333c8876c5e4387d2b3c2553f1ef12
Ahmed Magdi <128742811+Ahmed-Magdi1@users.noreply.github.com>
2025-07-11 01:38:39 +0300

    Adding ReadBoard file function

--- A_star/A_star.cpp
+++ A_star/A_star.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
-using namespace std;
+#include <vector>
+#include <string>
+#include <sstream>
+#include <fstream>
 
-int main() {
-    
-    return 0;
+using std::cout;
+using std::vector;
+using std::string;
+using std::ifstream;
+using std::istringstream;
+
+vector<int> ReadLine(string line){
+    istringstream Nline(line);
+    int n;
+    char k;
+    // string o = "1";
+    vector<int> row_vec;
+    while (Nline >> n >> k && k == ',') // When you find int followed by char that is equal to ','
+    {
+        if (n ==0){
+            row_vec.push_back(n);  // Empty
+        }  else {
+            row_vec.push_back(1); // Obstacles
+        }
+    }
+    return row_vec;
+}
+
+vector<vector<int>> ReadBoard(string path) {
+    ifstream gridfile(path);
+    vector<vector<int>> board{};
+    if (gridfile){
+        string j;
+        while (getline(gridfile, j))
+        {
+            vector<int> row = ReadLine(j);
+            board.push_back(row);
+        }
+    }
+    return board;
+}
+
+int main(){
+    auto board = ReadBoard("grid.txt"); // Path to the grid file
 }
