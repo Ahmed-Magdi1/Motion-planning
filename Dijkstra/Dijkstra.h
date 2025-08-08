f29e84081f80782d6f9105fa687590e9eb67ba52
Ahmed-Magdi1 <Ahmed216123@gmail.com>
2025-08-08 22:55:15 +0300

    adding the print function

---
+++ Dijkstra/Dijkstra.h
@@ -0,0 +1,13 @@
+#ifndef DIJKSTRA_H
+#define DIJKSTRA_H
+
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::vector;
+using std::endl;
+
+void PrintBoard(vector<vector<int>> Board);
+
+#endif
