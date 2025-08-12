a35cfb2fb9239b4b995222890d5f4cb13737083d
Ahmed-Magdi1 <Ahmed216123@gmail.com>
2025-08-12 21:17:54 +0300

    adding expandsearch function to search for the available nodes neighborhood

--- Dijkstra/Dijkstra.h
+++ Dijkstra/Dijkstra.h
@@ -8,12 +8,12 @@
 #include <vector>
 
 using std::cout;
-using std::endl;
 using std::ifstream;
 using std::istringstream;
 using std::string;
 using std::vector;
 
+const int neigborhood[4][2]{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
 enum class States { kEmpty, kObstacle, kClosed, kPath, kStart, kFinish };
 
 void PrintBoard(vector<vector<States>> Board);
@@ -21,5 +21,8 @@ vector<States> Readline(string line);
 vector<vector<States>> Readboard(string boardfile);
 string GridSymbol(States Symbol);
 bool checkvalidity(vector<int> point, vector<vector<States>> &board);
+void AddToOpen(int x, int y, int g, int x_pre, int y_pre, vector<vector<int>> &openlist, vector<vector<States>> &grid);
+void PrintBoardInt(vector<vector<int>> board);
+void expandsearch(vector<int> Point, vector<vector<States>> &grid, vector<vector<int>> &openlist);
 
 #endif
--- Dijkstra/Dijkstra_main.cpp
+++ Dijkstra/Dijkstra_main.cpp
@@ -4,13 +4,19 @@ int main() {
   int init[2] = {1, 3};
   int goal[2] = {3, 1};
   /* * * * * * * *  2D vector for testing * * * * * * * */
-  // vector<vector<int>> board{{1, 2, 3, 4, 5},
-  //                           {1, 2, 3, 4, 5},
-  //                           {1, 2, 3, 4, 5}};
-  // vector<int> testing {4,0,3,4};
+  vector<vector<int>> boardInt{{1, 2, 3, 4, 5},
+                               {1, 2, 3, 4, 5},
+                               {1, 2, 3, 4, 5}};
+  vector<int> testing {0, 0, 0};
   // PrintBoard(board); // Function to print a 2D vector
   auto board = Readboard("grid.board");
-  vector<int> test{init[0], init[1], goal[0], goal[1]};
-  checkvalidity(test, board);
-  PrintBoard(board);
+  // vector<int> test{init[0], init[1], goal[0], goal[1]};
+  // checkvalidity(test, board);
+  // PrintBoard(board);
+  // AddToOpen(1, 2, 7, 4, 5, boardInt, board);
+  // PrintBoardInt(boardInt);
+  // PrintBoard(board);
+  expandsearch(testing, board, boardInt);
+  PrintBoardInt(boardInt);
+  // PrintBoard(board);
 }
